8e8d0c7a988ecf07fda35498b082dc4e635b8c2a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-21 01:54:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,17 @@ class Student
   attr_accessor :name, :grade 
   attr_reader :id 
   
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
+    @id = id 
+  end 
   
-  
+  def create_table
+    
+  end 
+
+      
   
   
 end
